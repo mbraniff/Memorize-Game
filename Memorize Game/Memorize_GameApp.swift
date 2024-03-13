8c7f572be81d9b9f351f94330df8d379f6cd6833
mbraniff <mbraniff@alarm.com>
2024-03-13 14:00:53 -0600

    Initial Commit

---
+++ Memorize Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize Game
+//
+//  Created by Matthew Braniff on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
